c76dfeca4f554db7e096089c606ae48b2cc0b6fe
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-12-01 16:36:49 +0000

    style(DayDescription): add visual feedback on the ability of extendig descriptions

--- src/components/DayDescription.vue
+++ src/components/DayDescription.vue
@@ -7,21 +7,23 @@ const props = defineProps({
 
 const extended = ref(false);
 
-if (props.day.class.includes('active')){
+if (props.day.class.includes('active')) {
   extended.value = true;
 }
 </script>
 
 <template>
   <div :id="day.day" :class="'detail ' + day.class">
-    <div class="flex text-slate-500">
-      <span class="grow">Dec {{day.day}}</span>
-      <h4 :class="extended ? '' : 'rotate-180'" @click="extended = !extended">^</h4>
+    <div class="flex flex-col cursor-pointer group" @click="extended = !extended">
+      <div class="flex text-slate-500">
+        <span class="grow">Dec {{day.day}}</span>
+        <h4 :class="extended ? 'extend-btn' : 'extend-btn rotate-180'">^</h4>
+      </div>
+      <h3 class="grow mb-1 flex gap-2">
+        <span>{{day.title}}</span>
+        <span v-html="markdown.render(day.emoji)"></span>
+      </h3>
     </div>
-    <h3 class="grow mb-1 flex gap-2">
-      <span>{{day.title}}</span>
-      <span v-html="markdown.render(day.emoji)"></span>
-    </h3>
     <div v-if="extended" class="flex gap-1 mb-3">
         <span v-for="pill in day.tags" :key="pill" class="pill">
           {{pill}}
@@ -53,6 +55,6 @@ if (props.day.class.includes('active')){
 }
 
 .extend-btn {
-  @apply font-mono text-slate-300;
+  @apply font-mono text-slate-400 group-hover:text-slate-600 group-hover:font-semibold group-hover:animate-pulse;
 }
 </style>
